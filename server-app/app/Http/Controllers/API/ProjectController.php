f9ba5cf96dba0f6151dc629cbbda90416cc8a4f2
bsanaoui <bsanaoui@student.1337.ma>
2023-01-26 17:11:12 +0100

    Config Routes API for listing Projects and Cards and Route to Store new contact

--- server-app/app/Http/Controllers/API/ProjectController.php
+++ server-app/app/Http/Controllers/API/ProjectController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 
-class ProjectsController extends Controller
+class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
--- server-app/routes/api.php
+++ server-app/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\CardController;
+use App\Http\Controllers\API\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Config Route to only list data Projects (Get Method)
+Route::ApiResource('/projects', ProjectController::class)->only([
+    'index'
+]);
+
+// Config Route to only list data of Cards (Get Method)
+Route::ApiResource('/cards', CardController::class)->only([
+    'index'
+]);
+
+// Config Route to only list add new Contact (Post Method)
+Route::ApiResource('/contact_us', ContactController::class)->only([
+    'store'
+]);
