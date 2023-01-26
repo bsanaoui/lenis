e023d637b8a4076b6e775c2f1fa39052e69bfe94
bsanaoui <bsanaoui@student.1337.ma>
2023-01-26 16:31:21 +0100

    Create CardResource, ProjectResource and ContactResource

---
+++ server-app/app/Http/Resources/ProjectResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'source' => $this->source,
+            'href' => $this->href,
+          ];
+    }
+}
